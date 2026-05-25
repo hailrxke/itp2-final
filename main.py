0b3ac196d2d145109c3c3fe1e66a9df9dcddb833
saloverni <mubka2006@gmail.com>
2026-05-25 18:37:53 +0500

    Added interface Accountable for account.py and transactions.py. Some cleanup in transactions.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from models.transactions import Income, Expense
 from models.account import Account
+from models.categories import Category
 
 
 muba = Account(0)
-inc1 = Income(100, "horses", muba)
-inc2 = Income(1000, "loan", muba)
-exp1 = Expense(500, "food", muba)
+food = Category("food", 1000)
+inc1 = Income(100, muba)
+inc2 = Income(1000, muba)
+exp1 = Expense(500, food, muba)
 muba.add_transaction(inc1)
 muba.add_transaction(inc2)
 muba.add_transaction(exp1)
--- models/transactions.py
+++ models/transactions.py
@@ -1,35 +1,34 @@
 from abc import abstractmethod, ABC
-# from models.account import Account
 from datetime import datetime
+from models.categories import Category
+from models.interface import Accountable
 
 
 class Transaction(ABC):
-    def __init__(self, amount, category, account: "Account"):
+    def __init__(self, amount, account: Accountable):
         self.amount = amount
         self.date = datetime.now()
-        self.category = category
         self.account = account
+        if amount < 0:
+            raise ValueError(...)
 
     @abstractmethod
-    def apply(self):
-        pass
+    def apply(self): pass
 
 
 class Income(Transaction):
-    def __init__(self, amount, category, account: "Account"):
-        if amount < 0:
-            raise ValueError(...)
-        super().__init__(amount, category, account)
+    def __init__(self, amount, account: Accountable):
+        super().__init__(amount, account)
 
     def apply(self):
         self.account.deposit(self.amount)
 
 
 class Expense(Transaction):
-    def __init__(self, amount, category, account: "Account"):
-        if amount < 0:
-            raise ValueError(...)
-        super().__init__(amount, category, account)
+    def __init__(self, amount, category: Category, account: Accountable):
+        super().__init__(amount, account)
+        self.category = category
+        category.add_spent(amount)
 
     def apply(self):
         self.account.withdraw(self.amount)
