a5006a3a075ec5fa811429d1e81799271f84f6bb
Alikhan Seitmukhambetov <marmeladovasonyabladee@gmail.com>
2026-05-25 23:37:12 +0600

    created method category_expenses

--- services/analytics.py
+++ services/analytics.py
@@ -1,4 +1,4 @@
-from models.transactions import Transaction, Income
+from models.transactions import Transaction, Income, Expense
 
 class FinanceAnalyzer:
     def __init__(self, transactions: list[Transaction]):
@@ -6,6 +6,8 @@ class FinanceAnalyzer:
 
     def get_transactions(self):
         return self.__transactions
+    def set_transactions(self, transactions):
+        self.__transactions = transactions
 
     
     #if there 100000 transactions regular function load all to memory, generator gives one month at time and doesn't store rest
@@ -15,9 +17,19 @@ class FinanceAnalyzer:
             month = t.date[:7]
             if month not in monthes:
                 monthes[month] = {"income": 0, "expenses": 0}
-            if t.type == Income:
-                    monthes[month]["income"] += t.amount
+            if isinstance(t, Income):
+                monthes[month]["income"] += t.amount
             else:
-                    monthes[month]["expenses"] += t.amount
+                monthes[month]["expenses"] += t.amount
         for month, summary in monthes.items():
             yield month, summary
+
+    def category_expenses(self):
+        expenses = {}
+        for t in self.get_transactions():
+            if isinstance(t, Expense):
+                name = t.category.get_name()
+                if name not in expenses:
+                    expenses[name] = 0
+                expenses[name] += t.amount
+        return expenses
