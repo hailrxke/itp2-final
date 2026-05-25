e38268ca9100fdf071c29fcecd4b8faf13b04243
Alikhan Seitmukhambetov <marmeladovasonyabladee@gmail.com>
2026-05-25 18:38:22 +0600

    created method monthly_summary

--- services/analytics.py
+++ services/analytics.py
@@ -1,14 +1,23 @@
 from models.transactions import Transaction, Income
 
 class FinanceAnalyzer:
-    def init(self, transactions: list[Transaction]):
-        self.transactions = transactions
+    def __init__(self, transactions: list[Transaction]):
+        self.__transactions = transactions
 
-    def get_balance(self):
-        balance = 0
-        for t in self.transactions:
-            if isinstance(t, Income):
-                balance += t.amount
+    def get_transactions(self):
+        return self.__transactions
+
+    
+    #if there 100000 transactions regular function load all to memory, generator gives one month at time and doesn't store rest
+    def monthly_summary(self):
+        monthes = {}
+        for t in self.get_transactions():
+            month = t.date[:7]
+            if month not in monthes:
+                monthes[month] = {"income": 0, "expenses": 0}
+            if t.type == Income:
+                    monthes[month]["income"] += t.amount
             else:
-                balance -= t.amount
-        return balance
+                    monthes[month]["expenses"] += t.amount
+        for month, summary in monthes.items():
+            yield month, summary
